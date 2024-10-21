17bfa3c8765e75c213ef4fae4340013e55a02a25
jrPrzhe <119186022+jrPrzhe@users.noreply.github.com>
2024-10-21 09:42:32 +0800

    Update SeoHead.js

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -59,13 +59,13 @@ const SeoHead = (props) => {
       <meta property='og:site_name' content={meta.siteName} />
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content={meta.image} />
+      
       {/* Twitter */}
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:site' content='@F2aldi' />
       <meta name='twitter:title' content={meta.title} />
       <meta name='twitter:description' content={meta.description} />
-      <meta name='twitter:image' content={meta.image} />
+      
       {meta.date && (
         <>
           <meta property='article:published_time' content={meta.date} />
